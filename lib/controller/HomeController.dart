d50a22bb1324d3b7c51fcb7cce4d98e8c2f6bd23
QShutta <104342138+QShutta@users.noreply.github.com>
2023-04-12 10:54:32 +0300

    out

--- lib/controller/HomeController.dart
+++ lib/controller/HomeController.dart
@@ -7,7 +7,7 @@ class HomeController extends GetxController {
   //We make it from "obs" to be reactive so any change will hapen in it will be reflect in other places
   List questions = [].obs;
   //This var is to determine which page is going to bring the data from it in the api.and it's inital val is 1.
-  int page = 1;
+  var page = 1.obs;
   var scrollController = ScrollController().obs;
   //This is to check wither there is more data has been loading from the api.
   final RxBool isLoadingMore = false.obs;
@@ -15,7 +15,7 @@ class HomeController extends GetxController {
 //This method to get the data from the api.
   Future getData() async {
     var uri =
-        "https://api.stackexchange.com//2.3/questions?page=$page&order=desc&sort=activity&site=stackoverflow";
+        "https://api.stackexchange.com//2.3/questions?page=${page.value}&order=desc&sort=activity&site=stackoverflow";
     var res = await http.get(Uri.parse(uri));
     if (res.statusCode == 200) {
       var resBody = jsonDecode(res.body);
@@ -23,7 +23,7 @@ class HomeController extends GetxController {
       questions = questions + resBody['items'];
     } else {
       // If that call was not successful, throw an error.
-      throw Exception('Failed to load questions');
+      throw Exception('Failed to load questions:${res.statusCode}');
     }
     update();
   }
@@ -40,9 +40,8 @@ class HomeController extends GetxController {
     //If the position of the user in the end of the list,in that case he should load more data from the api.
     if (scrollController.value.position.pixels ==
         scrollController.value.position.maxScrollExtent) {
-     
       //In case of that the user arrive to the end of the page we have to load data from another page from the api
-      page = page + 1;
+      page.value = page.value + 1;
 
       //This var to indicate that ther is data come from the api
       isLoadingMore.value = true;
